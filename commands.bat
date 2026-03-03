fad0a4342c6e0984851f89ae7b756967682835cd
Atin Gupta <atingupta2005@gmail.com>
2026-03-03 10:47:05 +0530

    -

---
+++ commands.bat
@@ -0,0 +1,42 @@
+@echo off
+REM Windows batch script for creating the Elasticsearch KT folder structure
+REM Run this script from the root of the 18-ElasticSearch folder
+
+REM Data
+mkdir data\raw
+
+REM Docs
+mkdir docs
+
+REM Hands-on modules
+mkdir hands-on\module-00
+mkdir hands-on\module-01
+mkdir hands-on\module-02
+mkdir hands-on\module-03
+mkdir hands-on\module-04
+mkdir hands-on\module-05
+mkdir hands-on\module-06
+mkdir hands-on\module-07
+mkdir hands-on\module-08
+mkdir hands-on\module-09
+mkdir hands-on\module-10
+mkdir hands-on\module-11
+mkdir hands-on\Recycled
+
+REM Instructor prep
+mkdir instructor-prep\module-01
+mkdir instructor-prep\module-02
+mkdir instructor-prep\module-03
+mkdir instructor-prep\module-04
+mkdir instructor-prep\module-05
+mkdir instructor-prep\module-06
+mkdir instructor-prep\module-07
+mkdir instructor-prep\module-08
+mkdir instructor-prep\module-09
+mkdir instructor-prep\module-10
+mkdir instructor-prep\module-11
+mkdir instructor-prep\Recycled
+
+REM Prompts and references
+mkdir prompts
+mkdir references
